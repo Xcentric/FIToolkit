7ee986a6527a572989dd33187f3625858bf3abec
Xcentric <true.xcentric@gmail.com>
2015-07-02 18:06:29 +0300

    TestInsight™®© ready tests.

--- Tests/FIToolkitTests.dpr
+++ Tests/FIToolkitTests.dpr
@@ -15,17 +15,39 @@ program FIToolkitTests;
 {$ENDIF}
 
 uses
+  {$IFDEF USE_TESTINSIGHT}
+  TestInsight.DUnit,
+  TestInsight.Client,
+  {$ENDIF}
   DUnitTestRunner,
-  Test_Base.Exceptions in 'Test_Base.Exceptions.pas',
-  Base.Exceptions in '..\Source\Base\Base.Exceptions.pas',
-  Test_Config.FixInsight in 'Test_Config.FixInsight.pas',
-  Config.FixInsight in '..\Source\Config\Config.FixInsight.pas',
   Base.Consts in '..\Source\Base\Base.Consts.pas',
+  Base.Exceptions in '..\Source\Base\Base.Exceptions.pas',
+  Config.Consts in '..\Source\Config\Config.Consts.pas',
   Config.Exceptions in '..\Source\Config\Config.Exceptions.pas',
-  Config.Consts in '..\Source\Config\Config.Consts.pas';
+  Config.FixInsight in '..\Source\Config\Config.FixInsight.pas',
+  Test_Base.Exceptions in 'Test_Base.Exceptions.pas',
+  Test_Config.FixInsight in 'Test_Config.FixInsight.pas';
 
 {$R *.RES}
 
+  {$IFDEF USE_TESTINSIGHT}
+  function IsTestInsightRunning : Boolean;
+    var
+      TIClient : ITestInsightClient;
+  begin
+    TIClient := TTestInsightRestClient.Create;
+    TIClient.StartedTesting(0);
+    Result := not TIClient.HasError;
+  end;
+  {$ENDIF}
+
 begin
+  {$IFDEF USE_TESTINSIGHT}
+  if IsTestInsightRunning then
+    TestInsight.DUnit.RunRegisteredTests
+  else
+    DUnitTestRunner.RunRegisteredTests;
+  {$ELSE}
   DUnitTestRunner.RunRegisteredTests;
+  {$ENDIF}
 end.
