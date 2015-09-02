2000a8cdff2b282dd1370258eea17407e713a265
Xcentric <true.xcentric@gmail.com>
2015-09-02 18:32:03 +0300

    [*] nope, previous version had no chances

--- Source/Config/FIToolkit.Config.Defaults.pas
+++ Source/Config/FIToolkit.Config.Defaults.pas
@@ -3,59 +3,96 @@ unit FIToolkit.Config.Defaults;
 interface
 
 uses
-  System.Rtti,
+  System.Generics.Collections, System.Rtti,
   FIToolkit.Config.Types;
 
 type
+
+  { Base classes }
 
   TDefaultValueAttributeClass = class of TDefaultValueAttribute;
 
   TDefaultValueAttribute = class abstract (TCustomAttribute)
     strict private
-      FValue : TValue;
-      FValueKind : TDefaultValueKind;
+      FValueType : TDefaultValueKind;
     private
       function GetClassType : TDefaultValueAttributeClass;
-      function GetValue : TValue;
     protected
-      function CalculateValue : TValue; virtual;
+      constructor Create(AValueType : TDefaultValueKind);
     public
-      constructor Create(AValue : TValue); overload;
-      constructor Create; overload;
-
-      property Value : TValue read GetValue;
-      property ValueKind : TDefaultValueKind read FValueKind;
+      property ValueKind : TDefaultValueKind read FValueType;
   end;
 
-  DefaultValue = class (TDefaultValueAttribute);
+  TDefaultValueAttribute<T> = class abstract (TDefaultValueAttribute)
+    strict private
+      FValue : T;
+    private
+      function GetValue : T;
+    protected
+      function CalculateValue : T; virtual;
+    public
+      constructor Create(const AValue : T); overload;
+      constructor Create; overload;
+
+      property Value : T read GetValue;
+  end;
+
+  TDefaultsMap = class
+    strict private
+      type
+        TInternalMap = class (TDictionary<TDefaultValueAttributeClass, TValue>);
+    strict private
+      class var FStaticInstance : TDefaultsMap;
+
+      FInternalMap : TInternalMap;
+    private
+      class procedure FreeStaticInstance; static;
+      class function  GetStaticInstance : TDefaultsMap; static;
+    public
+      class property StaticInstance : TDefaultsMap read GetStaticInstance;
+
+      constructor Create;
+      destructor Destroy; override;
+
+      procedure AddValue(DefValAttribClass : TDefaultValueAttributeClass; Value : TValue);
+      function  GetValue(DefValAttribClass : TDefaultValueAttributeClass) : TValue;
+      function  HasValue(DefValAttribClass : TDefaultValueAttributeClass) : Boolean;
+  end;
 
   procedure RegisterDefaultValue(DefValAttribClass : TDefaultValueAttributeClass; Value : TValue);
 
 implementation
 
 uses
-  System.SysUtils, System.Generics.Collections;
-
-type
-
-  TDefaultsMap = class (TDictionary<TDefaultValueAttributeClass, TValue>)
-    strict private
-      class var FStaticInstance : TDefaultsMap;
-    private
-      class procedure FreeStaticInstance; static;
-      class function  GetStaticInstance : TDefaultsMap; static;
-    public
-      class property StaticInstance : TDefaultsMap read GetStaticInstance;
-  end;
+  System.SysUtils;
 
 { Utils }
 
 procedure RegisterDefaultValue(DefValAttribClass : TDefaultValueAttributeClass; Value : TValue);
 begin
-  TDefaultsMap.StaticInstance.Add(DefValAttribClass, Value);
+  TDefaultsMap.StaticInstance.AddValue(DefValAttribClass, Value);
 end;
 
 { TDefaultsMap }
+
+procedure TDefaultsMap.AddValue(DefValAttribClass : TDefaultValueAttributeClass; Value : TValue);
+begin
+  FInternalMap.Add(DefValAttribClass, Value);
+end;
+
+constructor TDefaultsMap.Create;
+begin
+  inherited Create;
+
+  FInternalMap := TInternalMap.Create;
+end;
+
+destructor TDefaultsMap.Destroy;
+begin
+  FreeAndNil(FInternalMap);
+
+  inherited Destroy;
+end;
 
 class procedure TDefaultsMap.FreeStaticInstance;
 begin
@@ -70,31 +107,23 @@ begin
   Result := FStaticInstance;
 end;
 
+function TDefaultsMap.GetValue(DefValAttribClass : TDefaultValueAttributeClass) : TValue;
+begin
+  Result := FInternalMap.Items[DefValAttribClass];
+end;
+
+function TDefaultsMap.HasValue(DefValAttribClass : TDefaultValueAttributeClass) : Boolean;
+begin
+  Result := FInternalMap.ContainsKey(DefValAttribClass);
+end;
+
 { TDefaultValueAttribute }
 
-constructor TDefaultValueAttribute.Create(AValue : TValue);
+constructor TDefaultValueAttribute.Create(AValueType : TDefaultValueKind);
 begin
   inherited Create;
 
-  FValue := AValue;
-  FValueKind := dvkData;
-end;
-
-constructor TDefaultValueAttribute.Create;
-begin
-  inherited Create;
-
-  FValue := TValue.Empty;
-  FValueKind := dvkCalculated;
-end;
-
-function TDefaultValueAttribute.CalculateValue : TValue;
-begin
-  with TDefaultsMap.StaticInstance do
-    if ContainsKey(Self.GetClassType) then
-      Result := Items[Self.GetClassType]
-    else
-      Result := TValue.Empty;
+  FValueType := AValueType;
 end;
 
 function TDefaultValueAttribute.GetClassType : TDefaultValueAttributeClass;
@@ -102,9 +131,36 @@ begin
   Pointer(Result) := PPointer(Self)^;
 end;
 
-function TDefaultValueAttribute.GetValue : TValue;
+{ TDefaultValueAttribute<T> }
+
+constructor TDefaultValueAttribute<T>.Create(const AValue : T);
 begin
-  Result := TValue.Empty;
+  inherited Create(dvkData);
+
+  FValue := AValue;
+end;
+
+constructor TDefaultValueAttribute<T>.Create;
+begin
+  inherited Create(dvkCalculated);
+end;
+
+function TDefaultValueAttribute<T>.CalculateValue : T;
+  var
+    V : TValue;
+begin
+  with TDefaultsMap.StaticInstance do
+    if HasValue(Self.GetClassType) then
+      V := GetValue(Self.GetClassType)
+    else
+      V := TValue.Empty;
+
+  Result := V.AsType<T>;
+end;
+
+function TDefaultValueAttribute<T>.GetValue : T;
+begin
+  Result := Default(T);
 
   case ValueKind of
     dvkData:
