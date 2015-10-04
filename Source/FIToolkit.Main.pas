93128f1829670ed6aff0c36eab772c10a8b13993
Xcentric <true.xcentric@gmail.com>
2015-10-05 00:29:29 +0300

    [*] integration of ready parts started

--- Source/FIToolkit.Main.pas
+++ Source/FIToolkit.Main.pas
@@ -5,15 +5,25 @@ interface
 uses
   System.SysUtils, System.Types;
 
-  procedure RunApplication(const FullExePath : TFileName; const LaunchParams : TStringDynArray);
+  procedure RunApplication(const FullExePath : TFileName; const CmdLineOptions : TStringDynArray);
 
 implementation
 
+uses
+  FIToolkit.CommandLine.Options, FIToolkit.CommandLine.Consts,
+  FIToolkit.Config.Manager;
+
 type
 
-  TFIToolkit = class
+  TFIToolkit = class sealed
+    strict private
+      FConfig : TConfigManager;
+      FOptions : TCLIOptions;
+    private
+      procedure InitConfig;
+      procedure InitOptions(const CmdLineOptions : TStringDynArray);
     public
-      constructor Create(const FullExePath : TFileName; const LaunchParams : TStringDynArray);
+      constructor Create(const FullExePath : TFileName; const CmdLineOptions : TStringDynArray);
       destructor Destroy; override;
 
       procedure Run;
@@ -21,11 +31,11 @@ type
 
 { Utils }
 
-procedure RunApplication(const FullExePath : TFileName; const LaunchParams : TStringDynArray);
+procedure RunApplication(const FullExePath : TFileName; const CmdLineOptions : TStringDynArray);
   var
     FIToolkit : TFIToolkit;
 begin
-  FIToolkit := TFIToolkit.Create(FullExePath, LaunchParams);
+  FIToolkit := TFIToolkit.Create(FullExePath, CmdLineOptions);
   try
     FIToolkit.Run;
   finally
@@ -35,18 +45,39 @@ end;
 
 { TFIToolkit }
 
-constructor TFIToolkit.Create(const FullExePath : TFileName; const LaunchParams : TStringDynArray);
+constructor TFIToolkit.Create(const FullExePath : TFileName; const CmdLineOptions : TStringDynArray);
 begin
   inherited Create;
 
-  //
+  InitOptions(CmdLineOptions);
+  InitConfig;
 end;
 
 destructor TFIToolkit.Destroy;
 begin
-  //
+  FreeAndNil(FConfig);
+  FreeAndNil(FOptions);
 
   inherited Destroy;
+end;
+
+procedure TFIToolkit.InitConfig;
+  var
+    SetConfigOption : TCLIOption;
+begin
+  //TODO: implement (with application state)
+  FOptions.Find(STR_CLI_OPTION_SET_CONFIG, SetConfigOption);
+  FConfig := TConfigManager.Create(SetConfigOption.Value, FOptions.Contains(STR_CLI_OPTION_GENERATE_CONFIG), True);
+end;
+
+procedure TFIToolkit.InitOptions(const CmdLineOptions : TStringDynArray);
+  var
+    S : String;
+begin
+  FOptions := TCLIOptions.Create;
+
+  for S in CmdLineOptions do
+    FOptions.Add(S);
 end;
 
 procedure TFIToolkit.Run;
