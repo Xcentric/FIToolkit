298d971dc7667d8d3f18fd001ca9146c16f4725c
Xcentric <true.xcentric@gmail.com>
2016-03-07 22:41:41 +0300

    [+] handy transition existence check [+] silent transition removing

--- Source/Base/FIToolkit.Base.FiniteStateMachine.pas
+++ Source/Base/FIToolkit.Base.FiniteStateMachine.pas
@@ -104,6 +104,8 @@ type
       procedure AfterExecute(const Command : TCommand); virtual;
       procedure BeforeExecute(const Command : TCommand); virtual;
       function  FindTransition(const FromState : TState; const OnCommand : TCommand) : TTransition;
+      function  HasTransition(const FromState : TState; const OnCommand : TCommand;
+        out Found : TTransition) : Boolean; overload;
     public
       constructor Create; overload; virtual; abstract;
       constructor Create(const InitialState : TState); overload; virtual;
@@ -341,9 +343,18 @@ begin
 end;
 
 function TFiniteStateMachine<TState, TCommand, ErrorClass>.HasTransition(const FromState : TState;
-  const OnCommand : TCommand) : Boolean;
+  const OnCommand : TCommand; out Found : TTransition) : Boolean;
 begin
-  //TODO: implement
+  Found  := FindTransition(FromState, OnCommand);
+  Result := Assigned(Found);
+end;
+
+function TFiniteStateMachine<TState, TCommand, ErrorClass>.HasTransition(const FromState : TState;
+  const OnCommand : TCommand) : Boolean;
+  var
+    T : TTransition;
+begin
+  Result := HasTransition(FromState, OnCommand, T);
 end;
 
 function TFiniteStateMachine<TState, TCommand, ErrorClass>.HasTransition(const OnCommand : TCommand) : Boolean;
@@ -353,10 +364,13 @@ end;
 
 function TFiniteStateMachine<TState, TCommand, ErrorClass>.RemoveTransition(const FromState : TState;
   const OnCommand : TCommand) : IFiniteStateMachine;
+  var
+    T : TTransition;
 begin
   Result := Self;
 
-  //TODO: implement
+  if HasTransition(FromState, OnCommand, T) then
+    FTransitionTable.Remove(T);
 end;
 
 end.
