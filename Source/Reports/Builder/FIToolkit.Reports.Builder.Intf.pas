d3f3f23242adab46db40569b95bfeccdf579886a
Xcentric <true.xcentric@gmail.com>
2016-10-03 16:41:27 +0300

    [+] initial version of report builder interface

--- Source/Reports/Builder/FIToolkit.Reports.Builder.Intf.pas
+++ Source/Reports/Builder/FIToolkit.Reports.Builder.Intf.pas
@@ -2,14 +2,17 @@
 
 interface
 
+uses
+  FIToolkit.Reports.Builder.Types;
+
 type
 
   IReportBuilder = interface
     ['{E5F3555E-A1B2-4087-AB75-F6A9DAB22627}']
-    procedure AddFooter();
-    procedure AddHeader();
-    procedure AddSummary();
-    procedure AppendRecord();
+    procedure AddFooter(FinishTime : TDateTime);
+    procedure AddHeader(const ProjectTitle : String; StartTime : TDateTime);
+    procedure AddSummary(const Items : array of TSummaryItem);
+    procedure AppendRecord(const Item : TReportRecord);
   end;
 
 implementation
--- Source/Reports/Builder/FIToolkit.Reports.Builder.Types.pas
+++ Source/Reports/Builder/FIToolkit.Reports.Builder.Types.pas
@@ -2,6 +2,23 @@
 
 interface
 
+type
+
+  TReportRecord = record
+    Column,
+    Line : Integer;
+    FileName,
+    MessageText,
+    MessageTypeKeyword,
+    MessageTypeName : String;
+  end;
+
+  TSummaryItem = record
+    MessageCount : Integer;
+    MessageTypeKeyword,
+    MessageTypeName : String;
+  end;
+
 implementation
 
 end.
