be885b2ee5173c2af976cb04187651569a6deb6a
Xcentric <true.xcentric@gmail.com>
2017-03-18 20:12:37 +0300

    [+] types, consts & tests

--- Source/Logger/FIToolkit.Logger.Consts.pas
+++ Source/Logger/FIToolkit.Logger.Consts.pas
@@ -2,6 +2,29 @@
 
 interface
 
+uses
+  FIToolkit.Logger.Types;
+
+const
+
+  { Severity consts }
+
+  SEVERITY_NONE = Low(TLogMsgSeverity);
+
+  SEVERITY_DEBUG   = TLogMsgSeverity(SEVERITY_NONE    + 1);
+  SEVERITY_INFO    = TLogMsgSeverity(SEVERITY_DEBUG   + 99);
+  SEVERITY_WARNING = TLogMsgSeverity(SEVERITY_INFO    + 100);
+  SEVERITY_ERROR   = TLogMsgSeverity(SEVERITY_WARNING + 100);
+  SEVERITY_FATAL   = TLogMsgSeverity(SEVERITY_ERROR   + 100);
+
+  SEVERITY_MAX = High(TLogMsgSeverity);
+
+  { Common consts }
+
+  ARR_MSGTYPE_TO_MSGSEVERITY_MAPPING : array [TLogMsgType] of TLogMsgSeverity = (
+    SEVERITY_NONE, SEVERITY_DEBUG, SEVERITY_INFO, SEVERITY_WARNING, SEVERITY_ERROR, SEVERITY_FATAL
+  );
+
 implementation
 
 end.
--- Source/Logger/FIToolkit.Logger.Types.pas
+++ Source/Logger/FIToolkit.Logger.Types.pas
@@ -2,6 +2,12 @@
 
 interface
 
+type
+
+  TLogMsgSeverity = type Word;
+
+  TLogMsgType = (lmNone, lmDebug, lmInfo, lmWarning, lmError, lmFatal);
+
 implementation
 
 end.
