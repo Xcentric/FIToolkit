804b5acfb4531b454299815674d8b574547df6b4
Xcentric <true.xcentric@gmail.com>
2017-03-19 16:00:42 +0300

    [+] logger interface

--- Source/Logger/FIToolkit.Logger.Intf.pas
+++ Source/Logger/FIToolkit.Logger.Intf.pas
@@ -2,6 +2,72 @@
 
 interface
 
+uses
+  System.SysUtils, System.Rtti,
+  FIToolkit.Logger.Types;
+
+type
+
+  ILogger = interface
+    ['{0E36214F-FFD4-4715-9631-0B6D7F12006A}']
+
+    { Property accessors }
+
+    function  GetSeverityThreshold : TLogMsgSeverity;
+    procedure SetSeverityThreshold(Value : TLogMsgSeverity);
+
+    { Logging: structure }
+
+    procedure EnterSection(const Msg : String = String.Empty); overload;
+    procedure EnterSection(const Vals : array of const); overload;
+    procedure EnterSectionFmt(const Msg : String; const Args : array of const);
+    procedure EnterSectionVal(const Vals : array of TValue);
+
+    procedure LeaveSection(const Msg : String = String.Empty); overload;
+    procedure LeaveSection(const Vals : array of const); overload;
+    procedure LeaveSectionFmt(const Msg : String; const Args : array of const);
+    procedure LeaveSectionVal(const Vals : array of TValue);
+
+    procedure EnterMethod(AClass : TClass; MethodAddress : Pointer; const Params : array of TValue);
+    procedure LeaveMethod(AClass : TClass; MethodAddress : Pointer; AResult : TValue);
+
+    { Logging: messages }
+
+    procedure Log(Severity : TLogMsgSeverity; const Msg : String); overload;
+    procedure Log(Severity : TLogMsgSeverity; const Vals : array of const); overload;
+    procedure LogFmt(Severity : TLogMsgSeverity; const Msg : String; const Args : array of const);
+    procedure LogVal(Severity : TLogMsgSeverity; const Vals : array of TValue);
+
+    procedure Debug(const Msg : String); overload;
+    procedure Debug(const Vals : array of const); overload;
+    procedure DebugFmt(const Msg : String; const Args : array of const);
+    procedure DebugVal(const Vals : array of TValue);
+
+    procedure Info(const Msg : String); overload;
+    procedure Info(const Vals : array of const); overload;
+    procedure InfoFmt(const Msg : String; const Args : array of const);
+    procedure InfoVal(const Vals : array of TValue);
+
+    procedure Warning(const Msg : String); overload;
+    procedure Warning(const Vals : array of const); overload;
+    procedure WarningFmt(const Msg : String; const Args : array of const);
+    procedure WarningVal(const Vals : array of TValue);
+
+    procedure Error(const Msg : String); overload;
+    procedure Error(const Vals : array of const); overload;
+    procedure ErrorFmt(const Msg : String; const Args : array of const);
+    procedure ErrorVal(const Vals : array of TValue);
+
+    procedure Fatal(const Msg : String); overload;
+    procedure Fatal(const Vals : array of const); overload;
+    procedure FatalFmt(const Msg : String; const Args : array of const);
+    procedure FatalVal(const Vals : array of TValue);
+
+    { Properties }
+
+    property SeverityThreshold : TLogMsgSeverity read GetSeverityThreshold write SetSeverityThreshold;
+  end;
+
 implementation
 
 end.
