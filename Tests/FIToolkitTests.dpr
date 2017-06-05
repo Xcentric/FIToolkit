562ff627243a21c7d7b236ad8d8129600e226271
Xcentric <true.xcentric@gmail.com>
2017-06-05 18:40:23 +0300

    [+] logger & log output tests skeleton

--- Tests/FIToolkitTests.dpr
+++ Tests/FIToolkitTests.dpr
@@ -76,6 +76,7 @@ uses
   Test_FIToolkit.Config.FixInsight in 'Test_FIToolkit.Config.FixInsight.pas',
   Test_FIToolkit.Config.Manager in 'Test_FIToolkit.Config.Manager.pas',
   Test_FIToolkit.Config.Storage in 'Test_FIToolkit.Config.Storage.pas',
+  Test_FIToolkit.Logger.Impl in 'Test_FIToolkit.Logger.Impl.pas',
   Test_FIToolkit.Logger.Types in 'Test_FIToolkit.Logger.Types.pas',
   Test_FIToolkit.Logger.Utils in 'Test_FIToolkit.Logger.Utils.pas',
   Test_FIToolkit.ProjectGroupParser.Parser in 'Test_FIToolkit.ProjectGroupParser.Parser.pas',
